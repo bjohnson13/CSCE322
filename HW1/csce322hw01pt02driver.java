c8f7052f2a3f879cd3fb63bbd2a8cf30032934ec
Brice Earl Johnson <bricejohnson@icloud.com>
2016-09-26 14:05:36 -0500

    HW1: Finished driver

--- HW1/csce322hw01pt02driver.java
+++ HW1/csce322hw01pt02driver.java
@@ -1,2 +1,29 @@
 import java.io.IOException;
 import org.antlr.v4.runtime.*;
+
+publics class csce322hw01pt02driver {
+  public static void main(String args[]){
+    CharStream input = null;
+
+    if( args.length == 1 ) {
+      try {
+        input = new ANTLRFileStream(args[0]);
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    } else {
+      System.err.println( "File Not Provided" );
+      System.exit(0);
+    }
+
+    csce322hw01pt02Lexer lexer = new csce322hw01pt02Lexer( input );
+    CommonTokenStream tokens = new CommonTokenStream( lexer );
+    csce322hw01pt02Parser parser = new csce322hw01pt02Parser( tokens );
+    lexer.removeErrorListeners();
+    parser.removeErrorListeners();
+    lexer.addErrorListener( new csce322hw01pt02error() );
+    parser.addErrorListener( new csce322hw01pt02error() );
+    parser.greaterThanSudoku();
+  }
+}
