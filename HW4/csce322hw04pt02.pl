aaf54be930dc4dd80a0ca8eea6a3cbc1cc44f06e
Brice Earl Johnson <bricejohnson@icloud.com>
2016-11-16 09:34:38 -0600

    HW4: Part 2 - Complete

--- HW4/csce322hw04pt02.pl
+++ HW4/csce322hw04pt02.pl
@@ -1,1 +1,20 @@
-columnsAndSpaces(Game).
+columnsAndSpaces(Game) :-
+  findall(Position, find2D(-, Game, Position), Positions),
+  length(Game,NumOfRows),
+  length(Positions, NumOfSpaces),
+  (
+  (0 is mod(NumOfRows, 2), 0 is mod(NumOfSpaces, 2));
+  (1 is mod(NumOfRows, 2), 1 is mod(NumOfSpaces, 2))
+  )
+  .
+% Code is examples provided by Ryan Patrick
+find2D(Element,[List|_],(0,Col)):-
+    find1D(Element,List,Col).
+find2D(Element,[_|Lists],(Row,Col)):-
+    find2D(Element,Lists,(RowT,Col)),
+    Row is RowT + 1.
+
+find1D(Element,[Element|_],0).
+find1D(Element,[_|T],Position):-
+    find1D(Element,T,PositionT),
+    Position is PositionT + 1.
